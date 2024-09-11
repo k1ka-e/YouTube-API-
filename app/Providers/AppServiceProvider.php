4cd75eff12e5fd61ea8900312cec41c2b6932918
Kristina <kzatesova@gmail.com>
2024-09-11 16:11:28 +0500

    Installing Laravel Sanctum (API Authentication)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,8 +25,5 @@ class AppServiceProvider extends ServiceProvider
         DB::listen(function ($query) {
             Log::info($query->sql);
         });
-
-        auth()->loginUsingId(1);
-
     }
 }
