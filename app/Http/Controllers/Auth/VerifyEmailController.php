5f01a2ba63668b5a93baa63785f6e7acba194214
Kristina <kzatesova@gmail.com>
2024-09-23 20:58:53 +0500

    Email confirmation when clicking on a link in an email

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 
 class VerifyEmailController extends Controller
 {
-    public function __invoke()
+    public function __invoke(EmailVerificationRequest $request)
     {
+        if ($request->user()->hasVerifiedEmail()) {
+            return response(['message' => 'Email has been already verified.']);
+        }
 
+        $request->user()->markEmailAsVerifed();
+
+        return  response(['message' => 'Email has been successfully verified.']);
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,7 @@ Route::post('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController
 Route::delete('/logout', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'destroy'])->middleware('auth');
 
 Route::post('/email/verification-notification', [\App\Http\Controllers\Auth\EmailVerificationNotificationController::class, 'store'])->middleware('auth');
-Route::get('/verify-email/{id}/{hash}', [\App\Http\Controllers\Auth\VerifyEmailController::class])->middleware('auth')->name('verification.verify');
+Route::get('/verify-email/{id}/{hash}', [\App\Http\Controllers\Auth\VerifyEmailController::class])
+    ->middleware(['auth', 'signed'])
+    ->name('verification.verify');
 
