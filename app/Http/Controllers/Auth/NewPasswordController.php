1d6242de400c3f64606e2c51c5934b8038d3cafd
Kristina <kzatesova@gmail.com>
2024-09-25 18:32:51 +0500

    Sending a link to reset your password

---
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
+
+class NewPasswordController extends Controller
+{
+    public function store(Request $request)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,10 @@ Route::get('/verify-email/{id}/{hash}', [\App\Http\Controllers\Auth\VerifyEmailC
     ->middleware(['auth', 'signed', 'throttle:6,1'])
     ->name('verification.verify');
 
+Route::get('/forgot-password', [\App\Http\Controllers\Auth\PasswordResetLinkController::class, 'store'])
+    ->middleware('guest');
+
+Route::get('/reset-password', [\App\Http\Controllers\Auth\NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.reset');
+
